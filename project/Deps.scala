0ede7b4fa093af5f42118a0750182a2f0f2851f3
Alberto Paro <alberto.paro@gmail.com>
2023-03-20 22:21:29 +0100

    Updated libraries

--- project/Deps.scala
+++ project/Deps.scala
@@ -12,11 +12,11 @@ object Deps {
   val javaDiff = "io.github.java-diff-utils" % "java-diff-utils" % "4.12"
   val scalaFmt = "org.scalameta" %% "scalafmt-core" % "3.7.2"
 
-  val jsoniterScala      = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.21.3"
-  val jsoniterScalaCirce = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-circe" % "2.21.3"
+  val jsoniterScala      = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.21.4"
+  val jsoniterScalaCirce = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-circe" % "2.21.4"
   // Use the "provided" scope instead when the "compile-internal" scope is not supported
   val jsoniterScalaMacros =
-    "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.21.3" % "provided"
+    "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.21.4" % "provided"
 
   val zioStreams        = "dev.zio" %% "zio-streams" % Versions.zio
   val collectionCompact = "org.scala-lang.modules" %% "scala-collection-compat" % "2.9.0"
@@ -29,6 +29,6 @@ object Deps {
 
 object Versions {
   val zio     = "2.0.10"
-  val zioJson = "0.4.2"
+  val zioJson = "0.5.0"
 
 }
